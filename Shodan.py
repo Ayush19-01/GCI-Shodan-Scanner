6f9c311a943b6a04dde586b1c35107c897909f38
Ayush19-01 <57313104+Ayush19-01@users.noreply.github.com>
2019-12-20 18:06:59 +0530

    Update Shodan.py

--- Shodan.py
+++ Shodan.py
@@ -3,48 +3,53 @@ import shodan
 import requests
 import os
 import socket
-from colorama import Fore, Back, Style
-from termcolor import colored
-
+def prRed(skk): return str("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): return str("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): return str("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): return str("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): return str("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): return str("\033[96m {}\033[00m" .format(skk)) 
+print("Welcome to the Shodan Scanner")
 key='p01uPcafWSzzcIJeeZFRhUwVsN4OaNIz'
 api=shodan.Shodan(key)
-
 def getip():
     print()
     r=requests.get("https://ifconfig.me/ip")
     print("Your Ip Address :"+" "+ r.text)
     print()
 def shodansearch():
-    os.system('cls')
-    word=input("Input your query to search : ")
-    r=api.search(word)
-    info = ''
-    for i in r['matches']:
-        if len(i["hostnames"])>0:
-            host1=i['hostnames'][0]
-        else:
-            host1=" "
-        print()
-        info+=colored(str(i['ip_str']),'red')+"       "+ colored(str(i['port']),"green") + "      "+ colored(str(i['isp']),'blue')+"        "+ colored(str(host1),'yellow')+ "\n"
-    pydoc.pager(info)
+	os.system('clear')
+	word=input("Input your query to search : ")
+	print("Press q to end")
+	r=api.search(word)
+	info=""
+	for i in r['matches'] :
+		if len(i["hostnames"])>0:
+			host1=i['hostnames'][0]
+		else:
+			host1=" "
+		print()
+		info+='{:28}{:18}{:65}{} \n'.format(prRed(str(i['ip_str'])),prGreen(str(i['port'])),prCyan(str(i['isp'])),prPurple(str(host1)))
+	print(info)
 def hostscan():
-    os.system('cls')
-    ip1=input('Enter domain:')
-    ip2=socket.gethostbyname(ip1)
-    r=api.host(ip2)
-    output = "\033[0;32m" + ip1 + '\033[0m\nHostnames:              '
-    for i in r['hostnames']:
-        output += (i + " " +"\n")
-        output +='City:                    {}\nCountry:                 {}\nOrganization:            {} \nNumber of open ports:    {} \nPorts:'.format(r['city'], r['country_name'], r['org'], len(r['ports']))
-    for i in r['data']:
-        if 'product' in i:
-            p=str(i['product'])
-        else:
-            p=" "
-        output+= ("    \033[1;34m{:<6}\033[0m  {}\n").format(str(i['port']), p)
-    pydoc.pager(output)
+	os.system('clear')
+	ip1=input('Enter domain:')
+	print(prYellow(ip1))
+	ip2=socket.gethostbyname(ip1)
+	r=api.host(ip2)
+	output='Hostnames:              '
+	for i in r['hostnames']:
+		output += (i + " " +"\n")
+	output +='City:                    {}\nCountry:                 {}\nOrganization:            {} \nNumber of open ports:    {} \nPorts:'.format(r['city'], r['country_name'], r['org'], len(r['ports']))
+	for j in r['data']:
+		if 'product' in i:
+			p=str(j['product'])
+		else:
+			p=" "
+		output+="                   "+str(j['port'])+"   "
+	print(output)
 while True:
-    print("Welcome to the Shodan Scanner")
+    
     print("""Choose from the following:\n1) Get Your IP\n2) Shodan search to scan Ip, port, host\n3) Scan a specific host\n4) Exit""")
     inp1=int(input("Shodan>"))
     if inp1==1:
